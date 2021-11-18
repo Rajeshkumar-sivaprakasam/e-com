16dc914c72f2c8555de8209dd306c1aa1ee0c02e
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2021-11-18 08:25:43 +0530

    products array added!

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Grid from '@material-ui/core';
+
+const products = [
+  {id:1, name:'shpoes', discription:'Running shoes.'},
+  {id:2, name:'MacBook', discription:'Apple macbook.'},
+]
+
+const Products = () => {
+  return (
+    <main>
+      <Grid containet justify='center' spacing={4}>
+
+      </Grid>
+    </main>
+  )
+}
+
+export default Products
