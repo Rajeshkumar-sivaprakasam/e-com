eba4645aae6e833f7562da9ad66fda48d2f15a81
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2021-11-18 20:06:30 +0530

    basic grid added!

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,21 +2,19 @@ import React from 'react'
 import {Grid} from '@material-ui/core';
 import { Product } from './Product/Product';
 const products = [
-  {id:1, name:'shpoes', discription:'Running shoes.'},
-  {id:2, name:'MacBook', discription:'Apple macbook.'},
+  {id:1, name:'shpoes', discription:'Running shoes.',price:'5$'},
+  {id:2, name:'MacBook', discription:'Apple macbook.',price:'10$'},
 ]
 
 export const Products = () => {
   return (
     <main>
-      <Grid containet justify='center' spacing={4}>
-        {products.map((product)=>{
-          <Grid item key={product.id} xs={12} sm={6} lg={3}>
-            <Product />
+      <Grid container justifyContent='center' spacing={2}>
+        {products.map((product)=>(
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product}/>
           </Grid>
-        })
-
-        }
+        ))}
       </Grid>
     </main>
   )
