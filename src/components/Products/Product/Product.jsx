7e41bed0f9cf23d9725c6ac3506c881cf86a0c43
Rajeshkumar Sivaprakasam <rajesh.kumar@goavega.com>
2021-11-18 08:41:01 +0530

    products with no-rerender!

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Product = () => {
+  return (
+    // {console.log('hello')}
+    <div>
+      Hello
+    </div>
+  )
+}
+
+
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import Grid from '@material-ui/core';
-
+import {Grid} from '@material-ui/core';
+import { Product } from './Product/Product';
 const products = [
   {id:1, name:'shpoes', discription:'Running shoes.'},
   {id:2, name:'MacBook', discription:'Apple macbook.'},
 ]
 
-const Products = () => {
+export const Products = () => {
   return (
     <main>
       <Grid containet justify='center' spacing={4}>
+        {products.map((product)=>{
+          <Grid item key={product.id} xs={12} sm={6} lg={3}>
+            <Product />
+          </Grid>
+        })
 
+        }
       </Grid>
     </main>
   )
 }
 
-export default Products
